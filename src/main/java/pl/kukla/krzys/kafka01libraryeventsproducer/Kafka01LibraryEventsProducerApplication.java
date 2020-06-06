b7cfee41c53179993ff0b8e2e78113d2ab215d14
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-06-06 17:18:31 +0200

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/kafka01libraryeventsproducer/Kafka01LibraryEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.kafka01libraryeventsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka01LibraryEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafka01LibraryEventsProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/kafka01libraryeventsproducer/Kafka01LibraryEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.kafka01libraryeventsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka01LibraryEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
